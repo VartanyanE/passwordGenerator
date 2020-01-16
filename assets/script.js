bf5c10a184f1d3ab2bea691d5d1eb8d07b062f35
mikevampm323 <mike.vartanyan@yahoo.com>
2020-01-15 16:41:12 -0800

    Added checkbox functionality

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,16 @@
 var passGen = document.querySelector("#passGen");
 var copyClip = document.querySelector("#copyClip");
-var passPrint = document.querySelector("#passPrint")
+var passPrint = document.querySelector("#passPrint");
 var numChar = [0, 1, 3, 4, 5, 6, 7, 8, 9];
-var specialChar = ['#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>',
+var specialCharecter = ['#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>',
     '?', '@'];
+// var toLowerCase = ranPassword.toLowerCase();
+// var toUpperCase = ranPassword.toUpperCase();
 var ranPassword = [];
-
+var lowercaseChar = document.querySelector("#lowercase");
+var uppercaseChar = document.querySelector("#uppercase");
+var specialChar = document.querySelector("#special");
+var numericChar = document.querySelector("#numeric");
 
 
 
@@ -13,7 +18,7 @@ for (let i = 0; i < 10; i++) {
     var ranNum = numChar[Math.floor(Math.random() * numChar.length)];
 }
 for (let i = 0; i < 10; i++) {
-    var ranChar = specialChar[Math.floor(Math.random() * specialChar.length)];
+    var ranChar = specialCharecter[Math.floor(Math.random() * specialCharecter.length)];
 }
 
 function mixedArr() {
@@ -22,9 +27,39 @@ function mixedArr() {
     passPrint.textContent = ranPassword;
 
 }
+function validateLower() {
+    if (document.getElementById('lowercase').checked) {
+        console.log("checked");
+    } else {
+        console.log("unchecked")
+    }
 
-if (document.getElementById("lowercase").checked === true) {
-    console.log("Checked")
+}
+
+function validateUpper() {
+    if (document.getElementById('uppercase').checked) {
+        console.log("checked");
+    } else {
+        console.log("unchecked")
+    }
+
+}
+function validateSpecial() {
+    if (document.getElementById('special').checked) {
+        console.log("checked");
+    } else {
+        console.log("unchecked")
+    }
+
+}
+
+function validateNumeric() {
+    if (document.getElementById('numeric').checked) {
+        console.log("checked");
+    } else {
+        console.log("unchecked")
+    }
+
 }
 
 
@@ -40,10 +75,26 @@ function copyToClip() {
 }
 
 
-passGen.addEventListener("click", (function () {
+passGen.addEventListener("click", function () {
     mixedArr();
-}));
+});
 
 copyClip.addEventListener("click", function () {
     copyToClip();
-})
+});
+
+lowercaseChar.addEventListener("click", function () {
+    validateLower();
+});
+
+uppercaseChar.addEventListener("click", function () {
+    validateUpper();
+});
+
+specialChar.addEventListener("click", function () {
+    validateSpecial();
+});
+
+numericChar.addEventListener("click", function () {
+    validateNumeric();
+});
